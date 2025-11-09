dbc837fefcf4e0ffad91e03c41a203e97a3fada4
JuanPabloRodriguezC <jprodriguezcano99@gmail.com>
2025-11-09 17:21:53 -0600

    pin on/off logic

--- gpio-driver.c
+++ gpio-driver.c
@@ -5,11 +5,37 @@
 #include <linix/proc_fs.h>
 #include <linux/slab.h>
 
+#include <asm/io.h>
+
 #define MAX_USER_SIZE 1024
+#define BCM2837_GPIO_ADDRESS 0x3F200000
 
 static struct proc_dir_entry *proc = NULL;
 
 static char data_buffer[MAX_USER_SIZE];
+
+static unsigned int *gpio_registers = NULL;
+
+static void gpio_pin_on(unsigned int pin)
+{
+    unsigned int reg = pin / 10;
+    unsigned int shift = (pin % 10) * 3;
+    unsigned int* gpio_fsel = gpio_registers + reg;
+    unsigned int* gpio_on_register = (unsigned int*)((char*)gpio_registers + 0x1C);
+
+    *gpio_fsel &= ~(7 << shift);
+    *gpio_fsel |= (1 << shift);
+    *gpio_on_register |= (1 << pin);
+
+    return;
+}
+
+static void gpio_pin_off(unsigned int pin)
+{
+    unsigned int* gpio_off_register = (unsigned int*)((char*)gpio_registers + 0x28);
+    gpio_off_register |= (1 << pin);
+    return;
+}
 
 ssize_t proc_read(struct file *file, char __user *buf, size_t count, loff_t *offset)
 {
@@ -20,6 +46,9 @@ ssize_t proc_read(struct file *file, char __user *buf, size_t count, loff_t *off
 
 ssize_t proc_write(struct file *file, const char __user *buf, size_t size, loff_t *offset)
 {
+    unsigned int pin = UINT_MAX;
+    unsigned int value = UINT_MAX;
+
     memset(data_buffer, 0x0, sizeof(data_buffer));
 
     if (size > MAX_USER_SIZE)
@@ -27,9 +56,38 @@ ssize_t proc_write(struct file *file, const char __user *buf, size_t size, loff_
         size = MAX_USER_SIZE;
     }
 
-    copy_from_user(data_buffer, buf, size);
+    if (copy_from_user(data_buffer, buf, size))
+        return 0;
 
-    printk("Read %zu bytes from /proc/gpio_driver\n", size);
+    if (sscanf(data_buffer, "%u %u", &pin, &value) != 2)
+    {
+        printk("Invalid input format. Use: <pin> <value>\n");
+        return size;
+    }
+
+    if (pin > 21 || pin <-0){
+        printk("Invalid pin number. Use a pin between 0 and 21.\n");
+        return size;
+    }
+
+    if (value != 0 && value != 1)
+    {
+        printk("Invalid value. Use 0 for LOW and 1 for HIGH.\n");
+        return size;
+    }
+    printk("You wrote: pin=%u value=%u\n", pin, value);
+
+    if (value == 1)
+    {
+        gpio_pin_on(pin);
+        printk("Set pin %u HIGH\n", pin);
+    }
+    else
+    {
+        gpio_pin_off(pin);
+        printk("Set pin %u LOW\n", pin);
+    }
+
     return size;
 }
 
@@ -40,7 +98,17 @@ static const struct proc_ops fops = {
 
 static int __init gpio_driver_init(void)
 {
-    printk("GPIO Driver Initialized\n");
+
+    gpio_registers = (int*)ioremap(BCM2837_GPIO_ADDRESSM PAGE_SIZE);
+
+    if (gpio_registers == NULL)
+    {
+        printk(KERN_ALERT "Error: Could not map GPIO registers\n");
+        return -ENOMEM;
+    }
+
+
+    printk("Successfully mapped in GPIO memory\n");
     proc = proc_create("gpio_driver", 0666, NULL, &fops);
     if (proc == NULL) {
         printk(KERN_ALERT "Error: Could not initialize /proc/gpio_driver\n");
